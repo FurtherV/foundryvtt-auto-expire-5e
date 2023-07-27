6f6a24ff02ef86ffe6d4586019bb9a3aaae1ab9d
FurtherV <alex.howel@web.de>
2023-07-27 07:19:47 +0200

    Initial commit

---
+++ src/scripts/constants.ts
@@ -0,0 +1,2 @@
+export const MODULE_ID = "typescript-template";
+export const TEMPLATE_FOLDER = `modules/${MODULE_ID}/templates`;
---
+++ src/scripts/main.ts
@@ -0,0 +1,8 @@
+/**
+ * @file Initial script executed by FoundryVTT on world load.
+ * The source code in this file is transpiled from TypeScript to JavaScript.
+ */
+
+function onInit() {}
+
+Hooks.once("init", onInit);
